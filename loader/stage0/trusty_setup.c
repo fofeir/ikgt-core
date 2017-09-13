613e5afb88e7dc912259e642469d5616817882ac
chengpx <pengx.cheng@intel.com>
2017-09-13 09:20:41 +0800

    add stage0 lib
    Change-Id: I26ec7582fad33cdb041318b29ee170cf5b36db58
    Signed-off-by: chengpx <pengx.cheng@intel.com>

--- loader/stage0/trusty_setup.c
+++ loader/stage0/trusty_setup.c
@@ -13,44 +13,52 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *******************************************************************************/
-
 #include "vmm_asm.h"
 #include "vmm_base.h"
 #include "vmm_arch.h"
 #include "evmm_desc.h"
-#include "trusty_info.h"
-#include "grub_boot_param.h"
-#include "guest_setup.h"
-#include "stage0_asm.h"
 #include "ldr_dbg.h"
+#include "trusty_info.h"
 
-static void fill_code32_seg(segment_t *ss)
+#define TRUSTY_BOOT_NULL     (0x00)
+#define TRUSTY_BOOT_CS       (0x08)
+#define TRUSTY_BOOT_DS       (0x10)
+
+void fill_code32_seg(segment_t *ss, uint16_t sel)
 {
 	ss->base = 0;
 	ss->limit = 0xffffffff;
 	ss->attributes = 0xc09b;
-	ss->selector = __BOOT_CS;
+	ss->selector = sel;
 }
 
-static void fill_data_seg(segment_t *ss)
+void fill_code64_seg(segment_t *ss, uint16_t sel)
+{
+	ss->base = 0;
+	ss->limit = 0xffffffff;
+	ss->attributes = 0xa09b;
+	ss->selector = sel;
+}
+
+void fill_data_seg(segment_t *ss, uint16_t sel)
 {
 	ss->base = 0;
 	ss->limit = 0xffffffff;
 	ss->attributes = 0xc093;
-	ss->selector = __BOOT_DS;
+	ss->selector = sel;
 }
 
-static void fill_tss_seg(segment_t *ss)
+void fill_tss_seg(segment_t *ss, uint16_t sel)
 {
 	ss->base = 0;
 	ss->limit = 0xffffffff;
 	ss->attributes = 0x808b;
 	/* it is ok for TR to be NULL while the attribute is not 0.
 	 * vmentry will not check it and guest OS will set the correct TR later */
-	ss->selector = __BOOT_NULL;
+	ss->selector = sel;
 }
 
-static boolean_t trusty_gcpu_setup(evmm_desc_t *xd)
+boolean_t trusty_gcpu_setup(evmm_desc_t *xd)
 {
 	trusty_desc_t *trusty_desc = &xd->trusty_desc;
 
@@ -63,7 +71,7 @@ static boolean_t trusty_gcpu_setup(evmm_desc_t *xd)
 	trusty_desc->gcpu0_state.cr4 = 0;
 
 	trusty_desc->gcpu0_state.msr_efer = 0x0;
-	fill_code32_seg(&trusty_desc->gcpu0_state.segment[SEG_CS]);
+	fill_code32_seg(&trusty_desc->gcpu0_state.segment[SEG_CS], TRUSTY_BOOT_CS);
 
 	/* it is ok for idtr to be 0. vmentry will not check it
 	 * and guest OS will set the correct idtr later */
@@ -72,12 +80,12 @@ static boolean_t trusty_gcpu_setup(evmm_desc_t *xd)
 
 	trusty_desc->gcpu0_state.segment[SEG_LDTR].attributes = 0x010000;
 
-	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_DS]);
-	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_ES]);
-	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_FS]);
-	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_GS]);
-	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_SS]);
-	fill_tss_seg(&trusty_desc->gcpu0_state.segment[SEG_TR]);
+	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_DS], TRUSTY_BOOT_DS);
+	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_ES], TRUSTY_BOOT_DS);
+	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_FS], TRUSTY_BOOT_DS);
+	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_GS], TRUSTY_BOOT_DS);
+	fill_data_seg(&trusty_desc->gcpu0_state.segment[SEG_SS], TRUSTY_BOOT_DS);
+	fill_tss_seg(&trusty_desc->gcpu0_state.segment[SEG_TR], TRUSTY_BOOT_NULL);
 
 	/* The guest RIP will be set in core since the relocation is done in core */
 	//trusty_desc->gcpu0_state.rip = 0;
@@ -87,8 +95,7 @@ static boolean_t trusty_gcpu_setup(evmm_desc_t *xd)
 
 	/* Trusty environment setup */
 	/* Stack resides at top of IMR region */
-	trusty_desc->gcpu0_state.gp_reg[REG_RSP] = trusty_desc->lk_file.runtime_addr +
-		trusty_desc->lk_file.runtime_total_size;
+	trusty_desc->gcpu0_state.gp_reg[REG_RSP] = trusty_desc->lk_file.runtime_addr + trusty_desc->lk_file.runtime_total_size;
 	/*
 	 * Trusty startup space just right after
 	 * memory sapce of trusty_device_info_t in IMR
@@ -100,13 +107,4 @@ static boolean_t trusty_gcpu_setup(evmm_desc_t *xd)
 	trusty_desc->gcpu0_state.rflags = 0x3002;
 
 	return TRUE;
-}
-
-boolean_t trusty_setup(evmm_desc_t *xd)
-{
-	/* setup secondary guests startup env */
-	if (trusty_gcpu_setup(xd)) {
-		return TRUE;
-	}
-	return FALSE;
 }
